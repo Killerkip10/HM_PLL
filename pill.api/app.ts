007c3803ac38046cdc264d38ba773b9c13dc57d6
uladzislau_pinchuk <uladzislau_pinchuk@epam.com>
2020-02-11 17:53:50 +0300

    feat(HM_PLL): add husky to pill.api

--- pill.api/app.ts
+++ pill.api/app.ts
@@ -1,8 +1,6 @@
 import * as express from 'express';
-import { Response } from 'express';
 
 const app = express();
-const ob = { name: ['vlad'] };
 
 app.get('/', (_, res) => res.send('May the force be with you'));
 
