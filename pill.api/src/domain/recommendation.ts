f7fc098b96da5f77e452a454bdbb0d2e90f7316f
uladzislau_pinchuk <uladzislau_pinchuk@epam.com>
2020-04-16 21:43:55 +0300

    feat(HM_PLL): fix eslint

--- pill.api/src/domain/recommendation.ts
+++ pill.api/src/domain/recommendation.ts
@@ -4,10 +4,10 @@ const ExactRecommendation = t.type({
   name: t.string,
 });
 
-const PartialRecommendation  = t.partial({
+const PartialRecommendation = t.partial({
   id: t.number,
 });
 
-export const Recommendation  = t.intersection([ExactRecommendation, PartialRecommendation]);
+export const Recommendation = t.intersection([ExactRecommendation, PartialRecommendation]);
 
-export type IRecommendation  = t.TypeOf<typeof Recommendation>;
+export type IRecommendation = t.TypeOf<typeof Recommendation>;
--- pill.api/src/infra/models/Medicine.ts
+++ pill.api/src/infra/models/Medicine.ts
@@ -12,14 +12,12 @@ import {
   ForeignKey,
   BelongsTo,
   DataType,
-  HasMany,
 } from 'sequelize-typescript';
 
 import { IMedicine } from '../../domain';
 import { Route } from './Route';
 import { TypeMedicine } from './TypeMedicine';
 import { ProductionMedicineMethod } from './ProductionMedicineMethod';
-import { MedicineConflict } from './MedicineConflict';
 
 @Table
 export class Medicine extends Model<Medicine> implements IMedicine {
